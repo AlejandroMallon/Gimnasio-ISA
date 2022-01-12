8ac04260487ca87da66483dd989135397d70433a
Ruben <irlandes007@gmail.com>
2022-01-12 12:35:48 +0100

    Version Final

--- app/src/main/java/gimnasio/app/AppApplication.java
+++ app/src/main/java/gimnasio/app/AppApplication.java
@@ -7,22 +7,19 @@ import java.util.UUID;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class AppApplication {
     public static ArrayList<Director> ldir = new ArrayList<Director>();
     public static List<Socio> lsocios = new ArrayList<>();
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
 
-        //TODO Registro socios + Controlador
-        //TODO Tabla Menu Director.
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
 
-        
-        
-
-
-		Director director = new Director ("11292716J");
+        Director director = new Director("11292716J");
+        director.setId(UUID.randomUUID());
+        director.setNombre("admin");
+        director.setContraseña("admin");
+        ldir.add(director);
         Socio socio1 = new Socio("socio@gimnasio.com");
         socio1.setNombre("Ruben");
         socio1.setTelefono("633293847");
@@ -35,13 +32,13 @@ public class AppApplication {
         socio2.setNumero_socio(UUID.randomUUID());
         lsocios.add(socio2);
 
-        director.setId(UUID.randomUUID());
         
+
         System.out.println("Id de director: " + director.getId());
-        System.out.println("Numero de socio: " + socio1.getNumero_socio());;
-	}
+        System.out.println("Numero de socio: " + socio1.getNumero_socio());
+        ;
+    }
 
 }
 
-//* Link tutorial: https://spring.io/guides/gs/serving-web-content/
-
+// * Link tutorial: https://spring.io/guides/gs/serving-web-content/
--- app/src/main/java/gimnasio/app/Controlador.java
+++ app/src/main/java/gimnasio/app/Controlador.java
@@ -1,8 +1,8 @@
 package gimnasio.app;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,8 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -24,106 +24,122 @@ public class Controlador {
 		return "greeting";
 	}
 
-		@GetMapping("/menuDirector")
-		public String menuDirector(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-				Model model) {
-			model.addAttribute("name", name);
-			return "menuDirector";
-		}
+	@GetMapping("/menuDirector")
+	public String menuDirector(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "menuDirector";
+	}
 
-		@GetMapping("/iniciarSesion")
-		public String iniciaSesion(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-				Model model) {
-			model.addAttribute("name", name);
-			return "iniciarSesion";
-		}
+	@GetMapping("/volver")
+	public String volver(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "volver";
+	}
 
-		@GetMapping("/registroSocio")
-		public String registroSocio(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-				Model model) {
-			model.addAttribute("name", name);
-			return "registroSocio";
-		}
+	@GetMapping("/iniciarSesion")
+	public String iniciaSesion(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "iniciarSesion";
+	}
 
-		@GetMapping("/error")
-		public String error(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-				Model model) {
-			model.addAttribute("name", name);
-			return "error";
-		}
+	@GetMapping("/registroSocio")
+	public String registroSocio(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "registroSocio";
+	}
 
-		@GetMapping("/tablaSocios")
-		public String tablaSocios(Model model) {
-			List<String> socioNombre = new ArrayList<>();
-			List<String> socioEmail = new ArrayList<>();
-			for(int i=0;i<AppApplication.lsocios.size();i++){
-				socioEmail.add(AppApplication.lsocios.get(i).getEmail());
-				socioNombre.add(AppApplication.lsocios.get(i).getNombre());
+	@GetMapping("/error")
+	public String error(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "error";
+	}
+
+	@GetMapping("/tablaSocios")
+	public String tablaSocios(Model model) {
+		List<String> socioNombre = new ArrayList<>();
+		List<String> socioEmail = new ArrayList<>();
+		for (int i = 0; i < AppApplication.lsocios.size(); i++) {
+			socioEmail.add(AppApplication.lsocios.get(i).getEmail());
+			socioNombre.add(AppApplication.lsocios.get(i).getNombre());
+		}
+		model.addAttribute("mensaje", "Hola buenas");
+		model.addAttribute("sociosEmail", socioEmail);
+		model.addAttribute("sociosNombre", socioNombre);
+		model.addAttribute("socios", AppApplication.lsocios);
+
+		return "tablaSocios";
+	}
+
+	@GetMapping("/eliminar")
+	public String eliminar(@RequestParam("id") UUID idSocio) {
+		System.out.println("Eliminando  socio con id: " + idSocio);
+		for (int i = 0; i < AppApplication.lsocios.size(); i++) {
+			if (AppApplication.lsocios.get(i).getNumero_socio().equals(idSocio)) {
+				AppApplication.lsocios.remove(i);
+				System.out.println("holaaaa " + idSocio);
+
+			} else {
+
 			}
-			model.addAttribute("mensaje", "Hola buenas");
-			model.addAttribute("sociosEmail", socioEmail);
-			model.addAttribute("sociosNombre", socioNombre);
-			model.addAttribute("socios", AppApplication.lsocios);
-
-
-			
-			return "tablaSocios";
 		}
+		return "delete";
+	}
 
-		@RequestMapping("validador")
-		public ModelAndView getValidaLogin(HttpServletRequest req, HttpServletResponse resp) {
-			ModelAndView modelo = new ModelAndView();
-			boolean encontrado = false;
-			String usuario = req.getParameter("u");
-			String password = req.getParameter("p");
-			for (int i = 0; i < AppApplication.ldir.size(); i++) {
-				if (usuario.equals(AppApplication.ldir.get(i).getNombre())
-						&& password.equals(AppApplication.ldir.get(i).getContraseña())) {
-					encontrado = true;
-					modelo.setViewName("/menuDirector");
-
-				}
+	@RequestMapping("validador")
+	public ModelAndView getValidaLogin(HttpServletRequest req, HttpServletResponse resp) {
+		ModelAndView modelo = new ModelAndView();
+		boolean encontrado = false;
+		String usuario = req.getParameter("u");
+		String password = req.getParameter("p");
+		for (int i = 0; i < AppApplication.ldir.size(); i++) {
+			if (usuario.equals(AppApplication.ldir.get(i).getNombre())
+					&& password.equals(AppApplication.ldir.get(i).getContraseña())) {
+				encontrado = true;
+				modelo.setViewName("menuDirector");
 
 			}
-			if (!encontrado) {
-				modelo.setViewName("/error");
-			}
-
-			return modelo;
-		}
-
-		@RequestMapping("altaDirector")
-		public ModelAndView getAltaDirector(HttpServletRequest req, HttpServletResponse resp) {
-			ModelAndView modelo = new ModelAndView();
-			String nombre = req.getParameter("nombre");
-			String correo = req.getParameter("correo");
-			String password = req.getParameter("p");
-
-			Director dir = new Director(nombre, correo, password);
-
-			AppApplication.ldir.add(dir);
-			modelo.setViewName("iniciarSesion");
-			return modelo;
-		}
-
-		@RequestMapping("altaSocio")
-		public ModelAndView getAltaSocio(HttpServletRequest req, HttpServletResponse resp) {
-			ModelAndView modelo = new ModelAndView();
-			String nombre = req.getParameter("nombre");
-			String telefono = req.getParameter("telefono");
-			String email = req.getParameter("email");
-			String cuenta_bancaria = req.getParameter("cuenta_bancaria");
-			String tipo_cuota = req.getParameter("tipo_cuota");
-			
-			
-
-			Socio socio = new Socio(nombre, telefono, email, cuenta_bancaria, tipo_cuota);
-
-			AppApplication.lsocios.add(socio);
-			modelo.setViewName("tablaSocios");
-			return modelo;
 
 		}
+		if (!encontrado) {
+			modelo.setViewName("error");
+		}
+
+		return modelo;
+	}
+
+	@RequestMapping("altaDirector")
+	public ModelAndView getAltaDirector(HttpServletRequest req, HttpServletResponse resp) {
+		ModelAndView modelo = new ModelAndView();
+		String nombre = req.getParameter("nombre");
+		String correo = req.getParameter("correo");
+		String password = req.getParameter("p");
+
+		Director dir = new Director(nombre, correo, password);
+
+		AppApplication.ldir.add(dir);
+		modelo.setViewName("iniciarSesion");
+		return modelo;
+	}
+
+	@RequestMapping("altaSocio")
+	public ModelAndView getAltaSocio(HttpServletRequest req, HttpServletResponse resp) {
+		ModelAndView modelo = new ModelAndView();
+		String nombre = req.getParameter("nombre");
+		String telefono = req.getParameter("telefono");
+		String email = req.getParameter("email");
+		String cuenta_bancaria = req.getParameter("cuenta_bancaria");
+		String tipo_cuota = req.getParameter("tipo_cuota");
+
+		Socio socio = new Socio(nombre, telefono, email, cuenta_bancaria, tipo_cuota);
+		socio.setNumero_socio(UUID.randomUUID());
+		AppApplication.lsocios.add(socio);
+		modelo.setViewName("volver");
+		return modelo;
+
+	}
 }
-
-
